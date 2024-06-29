e74e5f36758f8d45464b2c8ed76f76bc7c120785
Carlos López Gallardo <124450022+cegallardo0405@users.noreply.github.com>
2024-06-29 18:51:06 -0500

    chore: Update Astro configuration for serverless deployment

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://cegallardo0405.github.io",
   base: "/Krea-first-deploy",
+  output: 'server',
+  adapter: vercel(),
 });
