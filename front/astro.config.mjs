5ede610883fe489b45529c34b377366ba6e5746a
Carlos López Gallardo <124450022+cegallardo0405@users.noreply.github.com>
2024-06-29 21:49:15 -0500

    chore: Remove Vercel adapter from Astro configuration

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://cegallardo0405.github.io",
   base: "/Krea-first-deploy",
-  output: 'server',
-  adapter: vercel(),
 });
